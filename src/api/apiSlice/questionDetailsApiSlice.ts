79cd4eea8d815e7613e22884cf693a3fa64a2824
Лёша <airalex04@MacBook-Air-Lesa.local>
2026-08-04 15:39:01 +0300

    QestionDetailsPage fix

--- src/api/apiSlice/questionDetailsApiSlice.ts
+++ src/api/apiSlice/questionDetailsApiSlice.ts
@@ -1,36 +1,16 @@
-import type {
-    GetQuestionDetailsResponse,
-    Question
-} from "../../types/apiTypes";
-
+import type { Question } from "../../types/apiTypes";
 import { apiSlice } from "./apiSlice";
 
-
 export const questionDetailsApiSlice = apiSlice.injectEndpoints({
-
     endpoints: (builder) => ({
-
-        getQuestionDetails: builder.query<
-            Question,
-            string
-        >({
-
+        getQuestionDetails: builder.query<Question, string>({
             query: (id) =>
                 `/questions/public-questions/${id}`,
 
-            transformResponse: (
-                response: GetQuestionDetailsResponse
-            ) => response.data,
-
-
             providesTags: ["Questions"],
-
         }),
-
     }),
-
 });
-
 
 export const {
     useGetQuestionDetailsQuery,
